080611aee1f33f4d11ae3a9a411edd25240f9a08
FelixIzm <for_cv@mail.ru>
2020-02-28 17:30:41 +0300

    'work'

--- test.py
+++ test.py
@@ -14,43 +14,55 @@ service = build('drive', 'v3', credentials=credentials)
 
 #service.files().delete(fileId='0B4n1_ERzWhI7c3RhcnRlcl9maWxl').execute()
 
-root_results = service.files().list(pageSize=10,fields="nextPageToken, files(id, name, mimeType)",q="name contains 'Folder'").execute()
-#,q="name contains 'Folder1'"
-pp.pprint(root_results)
-id_parent_folder = root_results['files'][0]['id']
+name_root_folder = 'Folder' 
 
-nameFolder = 'Result_707876543'
+root_results = service.files().list(pageSize=10,fields="nextPageToken, files(id, name, mimeType)",q=Template("name contains '$name_root_folder'").safe_substitute(name_root_folder=name_root_folder)).execute()
+#pp.pprint(root_results)
+id_root_folder = root_results['files'][0]['id']
 
-result = service.files().list(pageSize=10,fields="nextPageToken, files(id, name, mimeType,webViewLink)",q=Template("name contains '$nameFolder'").safe_substitute(nameFolder=nameFolder)).execute()
+def save_to_folder(name_folder_save):
+    result = service.files().list(pageSize=10,fields="nextPageToken, files(id, name, mimeType,webViewLink)",q=Template("name contains '$name_folder_save'").safe_substitute(name_folder_save=name_folder_save)).execute()
+    if(not result['files']):
+        #create catalog
+        file_metadata = {
+            'name': name_folder_save,
+            'mimeType': 'application/vnd.google-apps.folder',
+            'parents': [id_root_folder]
+        }
+        result = service.files().create(body=file_metadata, fields='id').execute()
+    # id каталога для сохранения
+    id_folder_save = result['files'][0]['id']
+    # ссылка на каталог
+    web_link = result['files'][0]['webViewLink']
 
-if(not result['files']):
-    print('create catalog')
-    folder_id = id_parent_folder
-    file_metadata = {
-        'name': nameFolder,
-        'mimeType': 'application/vnd.google-apps.folder',
-        'parents': [folder_id]
-    }
-    result = service.files().create(body=file_metadata, fields='id').execute()
+    # получим список файлов в каталоге GoogleDrive, что бы не делать лишней работы
+    list_files = service.files().list(pageSize=1000,fields="nextPageToken, files(id, name, mimeType, parents)",q=Template("'$id_parents_folder' in parents").safe_substitute(id_parents_folder=id_folder_save)).execute()
+    control_list_file = []
+    for file in list_files['files']:
+        control_list_file.append(file['name'])
+    ################################################################################
+    return
+    tmpFolder='tmp/'
+    file_path = tmpFolder+'google.ico'
+    url = 'http://google.com/favicon.ico'
+    r = requests.get(url, allow_redirects=True)
+    f = open(file_path, 'wb')
+    f.write(r.content)
+    f.close()
+    name = 'google.ico'
+    file_path = file_path
+    file_metadata = {'name': name,'parents': [id_folder_save]}
+    media = MediaFileUpload(file_path, resumable=True)
+    r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    if(r['id']):
+        media = None
+        os.remove(file_path)
+    return web_link
+#################################
 
-id_folder_save = result['files'][0]['id']
+f_save = 'Result_707876543'
+save_to_folder(f_save)
 
-tmpFolder='tmp/'
-file_path = tmpFolder+'google.ico'
-pp.pprint(result['files'][0]['webViewLink'])
-url = 'http://google.com/favicon.ico'
-r = requests.get(url, allow_redirects=True)
-f = open(file_path, 'wb')
-f.write(r.content)
-f.close()
-name = 'google.ico'
-file_path = file_path
-file_metadata = {'name': name,'parents': [id_folder_save]}
-media = MediaFileUpload(file_path, resumable=True)
-r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-if(r['id']):
-    media = None
-    os.remove(file_path)
 
 
 '''
